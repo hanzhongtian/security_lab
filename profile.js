2f2cb829bc7ce2f27ca3c1c9ce70c5e0499c2598
hanzhongtian <524146476@qq.com>
2019-04-17 18:29:48 -0400

    Update handleProfileUpdate1
    to further prevent SQL injection attacks

--- profile.js
+++ profile.js
@@ -97,11 +97,10 @@ function handleProfileUpdate1(req,res,next,err,data)
    var bankAcc = req.body.bankAcc;
    var bankRouting = req.body.bankRouting;
 
-   var q = "UPDATE Profile SET ssn = '" + ssn + "', dob = '" + dob + "', address = '" +
-      address + "', bankAcc = '" + bankAcc + "', bankRouting = '" + bankRouting + "'" +
-      " WHERE userId = " + req.session.userId;
+   var q = "UPDATE Profile SET ssn = ?, dob = ?, address = ?, bankAcc = ?, bankRouting = ? WHERE userId = ?;
 
-   db.query(q,function(e1,d1) { handleProfileUpdate2(req,res,next,e1,d1); } );
+   db.query(q, [ssn, dob, address, bankAcc, bankRouting, req.session.userId], 
+            function(e1,d1) { handleProfileUpdate2(req,res,next,e1,d1); } );
 }
 
 
